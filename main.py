6b99a159481392a03c30c00ae69ab937ed41f833
lewis-github-1 <ltowne2@gmail.com>
2025-07-15 17:32:54 -0500

    Initial commit with black screen

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game = True
+    while game == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip() # Refresh the screen
+
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
