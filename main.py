d08ffec989b50323de2231a09cbb9414eb22bc22
lewis-github-1 <ltowne2@gmail.com>
2025-07-18 23:11:14 -0500

    added player rotational movement

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player1.update(dt)
 
         screen.fill((0, 0, 0))
         player1.draw(screen)
-
         pygame.display.flip() # Refresh the screen
+
         dt = clock.tick(60) / 1000
         
 
